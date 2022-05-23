57714623e20ff6da4a266a19794c6d87a811b528
Yousra Hiba <hiba.yousra.ouili@gmail.com>
2022-05-23 13:37:27 +0200

    -Add new class.

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 
 let quizData = require("./quiz_data.json");
 
-const Quiz = () => {
-  // return (  );
-};
-
+class Quiz extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
 export default Quiz;
